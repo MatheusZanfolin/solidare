909948ab38c349f76d32235e8016762029768a95
MatheusZanfolin <matheuszanfolin@hotmail.com>
2020-11-22 11:16:01 -0300

    Created base MVC classes

---
+++ Core/Architecture/View.cs
@@ -0,0 +1,15 @@
+﻿namespace Solidare.Core
+{
+    public abstract class View<V> where V : View<V>
+    {
+        private Controller<V> controller;
+
+        public View()
+        {
+            controller = CreateController();
+            controller.AttachTo(this);
+        }
+
+        public abstract Controller<V> CreateController();
+    }
+}
