1946f495b8b3ff25b3669d48d45007654259d97d
MatheusZanfolin <matheuszanfolin@hotmail.com>
2020-11-23 20:12:08 -0300

    Created database class

---
+++ Core/Database.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Solidare.Core
+{
+    public class Database
+    {
+        private readonly SqlConnection connection = new SqlConnection(Properties.Resources.ConnectionString);
+
+        public List<D> Get<D>(Operation get, Mapper<D> mapper, Parameters parameters = null)
+        {
+            var result = new List<D>();
+
+            PerformAtDatabase(() => {
+                var command = GetCommand(get, parameters);
+                
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        result.Add(mapper.Map(reader));
+                    }
+                }
+            });
+            
+            return result;
+        }
+
+        public void Insert(Operation insert)
+        {
+            PerformAtDatabase(() => { GetCommand(insert).ExecuteNonQuery(); });
+        }
+
+        public void Update(Operation update, Parameters parameters)
+        {
+            PerformAtDatabase(() => { GetCommand(update, parameters).ExecuteNonQuery(); });
+        }
+
+        public void Delete(Operation update, Parameters parameters)
+        {
+            PerformAtDatabase(() => { GetCommand(update, parameters).ExecuteNonQuery(); });
+        }
+
+        private void PerformAtDatabase(Action action)
+        {
+            connection.Open();
+
+            action();
+
+            connection.Close();
+        }
+
+        private SqlCommand GetCommand(Operation operation, Parameters parameters = null)
+        {
+            var command = new SqlCommand(operation.Query, connection);
+
+            if (parameters != null)
+            {
+                foreach (KeyValuePair<string, object> parameter in parameters.List)
+                {
+                    command.Parameters.Add(new SqlParameter(parameter.Key, parameter.Value));
+                }
+            }
+
+            return command;
+        }
+
+        public interface Operation
+        {
+            string Query { get; }
+        }
+
+        public interface Parameters
+        {
+            Dictionary<string, object> List { get; }
+        }
+
+        public interface Mapper<D>
+        {
+            D Map(SqlDataReader reader);
+        }
+    }
+}
